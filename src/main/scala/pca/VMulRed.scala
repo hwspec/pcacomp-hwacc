f482b6e2cdd98a651db2aca5c18f070b72fe0fe0
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2024-12-21 23:47:45 -0600

    reduction with minimum wiring

--- src/main/scala/pca/VMulRed.scala
+++ src/main/scala/pca/VMulRed.scala
@@ -4,25 +4,54 @@ import chisel3._
 import chisel3.util._
 import common.GenVerilog
 
+class LocalRedStage(n: Int = 4, inbw: Int = 8) extends Module {
+  require(n >= 2 && (n & (n - 1)) == 0) // check see if n is a power of two number that is larger than 4
 
-class VMulRed2(nbits_px: Int = 8, nbits_iem: Int = 8) extends Module {
-  val nbits_out = (nbits_px + nbits_iem)
   val io = IO(new Bundle {
-    val in_px = Input(UInt(nbits_px.W))
-    val in_iem = Input(SInt(nbits_iem.W)) // quantized inversed encoding matrix
-    val out = Output(SInt(nbits_out.W))
+    val in  = Input(Vec(n, SInt(inbw.W)))
+    val out = Output(Vec(n/2, SInt((inbw+1).W)))
   })
-  io.out := io.in_px * io.in_iem
+
+  for(i <- 0 until n/2) {
+    io.out(i) := io.in(i*2) +& io.in(i*2+1)
+  }
 }
 
-object VMulRed2 extends App {
-  val nbits_px=8
-  val nbits_iem=8
-
-  GenVerilog.generate(new VMulRed2(nbits_px,nbits_iem))
+object LocalRedStage extends App {
+  GenVerilog.generate(new LocalRedStage(n=4))
 }
 
+class LocalRedRec(n: Int = 4, inbw: Int = 8) extends Module {
+  require(n >= 2 && (n & (n - 1)) == 0) // check see if n is a power of two number that is larger than 4
 
+  val nstages = log2Ceil(n)
+  val outbw = inbw + nstages
+  val io = IO(new Bundle {
+    val in  = Input(Vec(n, SInt(inbw.W)))
+    val out = Output(SInt(outbw.W))
+  })
+
+  val stages = Array.tabulate(nstages) { i =>
+    Module(new LocalRedStage(n>>i, inbw+i))
+  }
+
+  // the first stage input wiring
+  for(i <- 0 until n)  stages(0).io.in(i) := io.in(i)
+
+  // connect output to input
+  for(s <- 1 until nstages) {
+    for(i <- 0 until n>>s) {
+      stages(s).io.in(i) := stages(s-1).io.out(i)
+    }
+  }
+
+  // the last stage output writing
+  io.out := stages(nstages-1).io.out(0)
+}
+
+object LocalRedRec extends App {
+  GenVerilog.generate(new LocalRedRec(n=4))
+}
 
 class VMulRed(n: Int = 4, nbits_px: Int = 8, nbits_iem: Int = 8) extends Module {
   require(n>=4 && (n&(n-1))==0) // check see if n is a power of two number that is larger than 4
