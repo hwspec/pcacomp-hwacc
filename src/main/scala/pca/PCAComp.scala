943d293b15c385c7947cc8a5ad52b5217c33d8d2
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2024-12-28 19:44:19 -0600

    multibank

--- src/main/scala/pca/PCAComp.scala
+++ src/main/scala/pca/PCAComp.scala
@@ -4,34 +4,40 @@ import chisel3._
 import chisel3.util._
 import common.GenVerilog
 
-class PCAComp(ninpixels: Int = 4096, pxbw: Int = 12, maxenc: Int = 30, encbw: Int = 8 ) extends Module {
+
+class PCAComp(ninpixels: Int = 256, ncolumns : Int = 16, pxbw: Int = 8, maxenc: Int = 30, encbw: Int = 8 ) extends Module {
   val io = IO(new Bundle {
     val in = Flipped(Decoupled(Vec(ninpixels, UInt(pxbw.W))))
     val out = Decoupled(Vec(maxenc, SInt(encbw.W)))
     //
-    val loadenc = Input(Bool()) // load encdata into encmat at encpos
-    val verifyenc = Input(Bool()) // load encdata into encmat at encpos
+    val setencdata = Input(Bool()) // load encdata into encmat at encpos
+    val getencdata = Input(Bool()) // load encdata into encmat at encpos
     val encpos  = Input(UInt(log2Ceil(ninpixels).W))
-    val encdata = Input(Vec(maxenc, SInt(encbw.W)))
-    val encdataverify = Output(Vec(maxenc, SInt(encbw.W)))
+    val encdata = Input(Vec(ninpixels,Vec(maxenc, SInt(encbw.W))))
+    val encdataverify = Output(Vec(ninpixels,Vec(maxenc, SInt(encbw.W))))
   })
 
   io.in.ready := false.B
   io.out.valid := false.B
   for (i <- 0 until maxenc) io.out.bits(i) := 0.S
 
-  val encmat = SyncReadMem(ninpixels, Vec(maxenc, SInt(encbw.W)))
-  when (io.loadenc) {
-    encmat(io.encpos) := io.encdata
-  }
-  when (io.verifyenc) {
-    io.encdataverify := encmat(io.encpos)
-  }.otherwise {
-    for (i <- 0 until maxenc) {
-      io.encdataverify(i) := 0.S
+  val encmat = Seq.fill(ninpixels) {SyncReadMem(ncolumns, Vec(maxenc, SInt(encbw.W)))}
+  when (io.setencdata) {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      encmat(b)(io.encpos) := io.encdata(b)
     }
   }
-
+  when (io.getencdata) {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      io.encdataverify(b) := encmat(b)(io.encpos)
+    }
+  }.otherwise {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      for (i <- 0 until maxenc) {
+        io.encdataverify(b)(i) := 0.S
+      }
+    }
+  }
 }
 
 object PCAComp extends App {
--- src/test/scala/pca/PCACompSpec.scala
+++ src/test/scala/pca/PCACompSpec.scala
@@ -8,37 +8,44 @@ import common.CommonSpecConfig
 class PCACompSpec extends CommonSpecConfig {
   behavior of "PCAComp"
   val ninpixels = 16 // 4096
+  val ncolumns = 8
   val pxbw = 12
   val maxenc = 4 // 30
   val encbw = 8
 
   "encmat load" should "pass" in {
-    test(new PCAComp(ninpixels, pxbw, maxenc, encbw)) { dut =>
+    test(new PCAComp(ninpixels, ncolumns, pxbw, maxenc, encbw)) { dut =>
       // set
-      dut.io.loadenc.poke(true)
-      for(pos <- 0 until ninpixels) {
-        dut.io.encpos.poke(pos)
-        for(i <- 0 until maxenc) {
-          val v = (pos+i) % (1<<(encbw-1))
-          dut.io.encdata(i).poke(v)
+      dut.io.setencdata.poke(true)
+      for(col <- 0 until ncolumns) {
+        dut.io.encpos.poke(col)
+        for(b <- 0 until ninpixels) {
+          for (i <- 0 until maxenc) {
+            val v = (col + b + i) % (1 << (encbw - 1))
+            dut.io.encdata(b)(i).poke(v)
+          }
         }
         dut.clock.step()
       }
-      dut.io.loadenc.poke(false)
+      dut.io.setencdata.poke(false)
 
       // verify
-      dut.io.verifyenc.poke(true)
-      for(pos <- 0 until ninpixels) {
-        dut.io.encpos.poke(pos)
+      dut.io.getencdata.poke(true)
+      for(col <- 0 until ncolumns) {
+        dut.io.encpos.poke(col)
         dut.clock.step() // due to syncreadram
-        for(i <- 0 until maxenc) {
-          val v = (pos+i) % (1<<(encbw-1))
-          dut.io.encdataverify(i).expect(v)
-          // val ret = dut.io.encdataverify(i).peekInt()
-          // println(f"$pos $i $v $ret")
+        for(b <- 0 until ninpixels) {
+          for (i <- 0 until maxenc) {
+            val v = (col + b + i) % (1 << (encbw - 1))
+            dut.io.encdata(b)(i).poke(v)
+            dut.io.encdataverify(b)(i).expect(v)
+//            val ret = dut.io.encdataverify(b)(i).peekInt()
+//            println(f"[$col:$b:$i] $v should be $ret")
+          }
         }
+        dut.clock.step()
       }
-      dut.io.verifyenc.poke(false)
+      dut.io.getencdata.poke(false)
     }
   }
 }
