da1e7054772ac9a26d43b2444f8240d22c7d2a7d
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2024-12-31 21:34:29 -0600

    checked synthezability

--- src/main/scala/pca/PCAComp.scala
+++ src/main/scala/pca/PCAComp.scala
@@ -8,44 +8,75 @@ import common.GenVerilog
 // design parameters for PCA-based compressor
 //
 // * input images
+// blockid: the PCA encoder block ID
 // pxbw: pixel bit width (unsigned int)
-// width : the input image width
-// height : its height
-// ngroups: shift groups
-// nblocks: the number of sub-PCA blocks
+// width: the number of the image columns that are shifted every cycles
+// height : per-block height
 //
 // * pca
 // encsize : the maximum encoding size
 // encbw : encoding bit width (signed int)
 // qfactors: quantization factor (vector) needed on chip?
 //
-class PCACompTop(
-                // pixel-sensor params
-                pxbw: Int = 12, width: Int = 256, height: Int = 256,
-                ngroups: Int = 16,
-                nblocks: Int = 8, // the number of sub-PCA blocks
-                // PCA params
-                encsize: Int = 30, // the maximum encoding size
-                encbw : Int = 8 // encoding bit width (signed int)
-                ) extends Module {
+class PCACompBlock(
+                    blockid: Int = 0,
+                    // pixel-sensor params
+                    pxbw: Int = 12, width: Int = 16, height: Int = 32,
+
+                    // PCA params
+                    encsize: Int = 30, // the maximum encoding size
+                    encbw : Int = 8 // encoding bit width (signed int)
+                  ) extends Module {
+  val ninpixels = (width * height)
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(Vec(ninpixels, UInt(pxbw.W))))
+    val out = Decoupled(Vec(encsize, SInt(encbw.W))) // compressed data
+    //
+    val setencdata = Input(Bool()) // load encdata into encmat at encpos
+    val getencdata = Input(Bool()) // load encdata into encmat at encpos
+    val encpos = Input(UInt(log2Ceil(ninpixels).W))
+    val encdata = Input(Vec(ninpixels, Vec(encsize, SInt(encbw.W))))
+    val encdataverify = Output(Vec(ninpixels, Vec(encsize, SInt(encbw.W))))
+  })
+
+  io.in.ready := false.B
+  io.out.valid := false.B
+  for (i <- 0 until encsize) io.out.bits(i) := 0.S
+
+  val encmat = Seq.fill(ninpixels) {SyncReadMem(width, Vec(encsize, SInt(encbw.W)))}
+  when (io.setencdata) {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      encmat(b)(io.encpos) := io.encdata(b)
+    }
+  }
+  when (io.getencdata) {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      io.encdataverify(b) := encmat(b)(io.encpos)
+    }
+  }.otherwise {
+    for (b <- 0 until ninpixels) { // ninpixels banks
+      for (i <- 0 until encsize) {
+        io.encdataverify(b)(i) := 0.S
+      }
+    }
+  }
+
 
 }
 
-object PCACompTop extends App {
+object PCACompBlock extends App {
+  val blockid : Int = 0
   val pxbw: Int = 12
-  val width: Int = 256
-  val height: Int = 256
-  val ngroups: Int = 16
-  val nblocks: Int = 8
+  val width: Int = 16
+  val height: Int = 16
   val encsize: Int = 30
   val encbw : Int = 8
 
-  GenVerilog.generate(new PCACompTop(
+  GenVerilog.generate(new PCACompBlock(
+    blockid = blockid,
     pxbw = pxbw,
     width = width,
     height = height,
-    ngroups = ngroups,
-    nblocks = nblocks,
     encsize = encsize,
     encbw = encbw
   ))
