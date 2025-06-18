c8a9a16881d671a9a2c5337a61500eaeab0e9e1c
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2025-06-18 17:51:32 -0400

    adding split

--- src/test/scala/pca/PCACompBlockSpec.scala
+++ src/test/scala/pca/PCACompBlockSpec.scala
@@ -13,16 +13,20 @@ import chisel3.simulator.EphemeralSimulator._
  * vec : row-vector with the size n.
  * mat : matrix with n rows and m cols.
  *
- * @param n the number of the rows in mat and the length of vec
- * @param m the number of the columns in vec
+ * @param w the width  of the input pixel matrix
+ * @param h the height of the input pixel matrix
+ * @param m the number of the columns in vec, which is the number of principal components
  * @param vecbw : the bitwidth of vec element (unsigned integer)
  * @param matbw : the bitwidth of mat element (signed integer)
  * @param nblocks : the number of the parallel encoding blocks
  *
  */
-class PCATestData(val n: Int = 168, val m: Int = 192, vecbw: Int = 12, matbw: Int = 8, nblocks : Int = 8) {
+class PCATestData(val w: Int = 24, val h : Int = 20, val m: Int = 10, vecbw: Int = 10, matbw: Int = 8, nblocks : Int = 2) {
+  val n = w * h
   val resbw = vecbw + matbw + log2Ceil(n)
   require(resbw < 64)
+
+  val blockwidth = w / nblocks
 
   val rnd = new Random(123)
 
@@ -30,6 +34,21 @@ class PCATestData(val n: Int = 168, val m: Int = 192, vecbw: Int = 12, matbw: In
     val tmp = 1 << matbw
     rnd.between(-tmp, tmp)
   }
+  import scala.reflect.ClassTag
+
+  def hsplit[T: ClassTag](a: Array[Array[T]], numSplits: Int): Array[Array[Array[T]]] = {
+    val rows = a.length
+    val cols = a(0).length
+    require(cols % numSplits == 0, "Number of columns must be divisible by numSplits")
+
+    val splitWidth = cols / numSplits
+    Array.tabulate(numSplits) { splitIdx =>
+      Array.tabulate(rows) { row =>
+        a(row).slice(splitIdx * splitWidth, (splitIdx + 1) * splitWidth)
+      }
+    }
+  }
+
 
   val vec: Array[Long] = Array.fill(n) {
     rnd.nextInt(1 << vecbw)
@@ -39,6 +58,19 @@ class PCATestData(val n: Int = 168, val m: Int = 192, vecbw: Int = 12, matbw: In
   for (encidx <- 0 until m) {
     ref(encidx) = ref(encidx) +
       (0 until n).map(j => vec(j) * mat(j)(encidx)).sum
+  }
+}
+
+object Temp extends App {
+  val a = Array(
+    Array(1.0, 2.0, 3.0, 4.0),
+    Array(5.0, 6.0, 7.0, 8.0)
+  )
+  val t = new PCATestData()
+  val result = t.hsplit(a, 2)
+  result.foreach { part =>
+    println("Split:")
+    part.foreach(row => println(row.mkString("[", ", ", "]")))
   }
 }
 
