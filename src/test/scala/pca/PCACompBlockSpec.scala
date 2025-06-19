a9b73d0cdd1d194f8cef69990650e4a65e8189ac
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2025-06-18 20:07:02 -0400

    default and large config passed

--- src/test/scala/pca/PCACompBlockSpec.scala
+++ src/test/scala/pca/PCACompBlockSpec.scala
@@ -21,56 +21,137 @@ import chisel3.simulator.EphemeralSimulator._
  * @param nblocks : the number of the parallel encoding blocks
  *
  */
-class PCATestData(val w: Int = 24, val h : Int = 20, val m: Int = 10, vecbw: Int = 10, matbw: Int = 8, nblocks : Int = 2) {
+class PCATestData(val w: Int = 12, val h : Int = 3, val m: Int = 7, vecbw: Int = 9, matbw: Int = 8, nblocks : Int = 3, seed : Int = 123) {
+  import scala.reflect.ClassTag
+
   val n = w * h
   val resbw = vecbw + matbw + log2Ceil(n)
   require(resbw < 64)
 
   val blockwidth = w / nblocks
 
-  val rnd = new Random(123)
+  val rnd = new Random(seed)
 
-  val mat: Array[Array[Long]] = Array.fill(n, m) {
+  val img : Array[Array[Long]] = Array.fill(w, h) {
+    rnd.nextInt(1 << vecbw)
+  }
+  val vec: Array[Long] = img.flatten
+
+  // mat is a transposed invenc matrix
+  val mat: Array[Array[Long]] = Array.fill(m, n) {
     val tmp = 1 << matbw
     rnd.between(-tmp, tmp)
   }
-  import scala.reflect.ClassTag
 
-  def hsplit[T: ClassTag](a: Array[Array[T]], numSplits: Int): Array[Array[Array[T]]] = {
-    val rows = a.length
-    val cols = a(0).length
-    require(cols % numSplits == 0, "Number of columns must be divisible by numSplits")
+  val ref: Array[Long] = Array.fill(m)(0.toLong)
+  for (encidx <- 0 until m) {
+    ref(encidx) = (0 until n).map(j => vec(j) * mat(encidx)(j)).sum
+  }
 
-    val splitWidth = cols / numSplits
-    Array.tabulate(numSplits) { splitIdx =>
-      Array.tabulate(rows) { row =>
-        a(row).slice(splitIdx * splitWidth, (splitIdx + 1) * splitWidth)
+  // split vec and mat block-wise
+  //
+  // blockvec(blockid)(rowid)(pixpos)
+  val blockvec : Array[Array[Array[Long]]] =
+    Array.tabulate(nblocks) { blockpos =>
+      Array.tabulate(h) { rowpos =>
+        vec.slice(rowpos * w + blockpos * blockwidth, rowpos * w + (blockpos + 1) * blockwidth)
+      }
+    }
+
+  // blockmat(encid)(blockid)(rowid)(pixpos)
+  val blockmat :  Array[Array[Array[Array[Long]]]] =
+    Array.tabulate(m) { encpos =>
+      Array.tabulate(nblocks) { blockpos =>
+        Array.tabulate(h) { rowpos =>
+          mat(encpos).slice(rowpos * w + blockpos * blockwidth, rowpos * w + (blockpos + 1) * blockwidth)
+        }
+      }
+    }
+
+  def dumpVec(): Unit = {
+    println("[Flatten Input Vec]")
+    println("  " + vec.mkString("[", ", ", "]"))
+    println()
+    println("[Grouped Input Vec]")
+    for ((b,bid) <- blockvec.zipWithIndex) {
+      println(s" [block${bid}]")
+      for ((r,rid) <- b.zipWithIndex) {
+        print(s"   $rid : ")
+        println(r.mkString("[", ", ", "]"))
+      }
+    }
+    println()
+  }
+
+  def dumpMat(): Unit = {
+    println("[Transposed IEM]")
+    for ((r, rid) <- mat.zipWithIndex) {
+      print(s"   $rid : ")
+      println(r.mkString("[", ", ", "]"))
+    }
+    println()
+    println("[Grouped IEM]")
+    for ((e, eid) <- blockmat.zipWithIndex) {
+      println(s" [enc${eid}]")
+      for ((b, bid) <- e.zipWithIndex) {
+        println(s"   [block${bid}]")
+        for ((r, rid) <- b.zipWithIndex) {
+          print(s"      $rid : ")
+          println(r.mkString("[", ", ", "]"))
+        }
       }
     }
   }
 
 
-  val vec: Array[Long] = Array.fill(n) {
-    rnd.nextInt(1 << vecbw)
+  def calcRefFromBlocks() : Array[Long] = {
+    var tmp: Array[Long] = Array.fill(m)(0)
+
+    for (eid <- 0 until m) {
+      for (bid <- 0 until nblocks) {
+        for (rid <- 0 until h) {
+          tmp(eid) +=
+            Array.tabulate(blockwidth) { i =>
+              blockvec(bid)(rid)(i) * blockmat(eid)(bid)(rid)(i)
+            }.sum
+        }
+      }
+    } // blockmat(encid)(blockid)(rowid)(pixpos)
+    tmp
   }
 
-  val ref: Array[Long] = Array.fill(m)(0.toLong)
-  for (encidx <- 0 until m) {
-    ref(encidx) = ref(encidx) +
-      (0 until n).map(j => vec(j) * mat(j)(encidx)).sum
+
+  def validateRef() : Boolean = {
+    var tmp = true
+    val reffromblocks = calcRefFromBlocks()
+    for (eid <- 0 until m) {
+      if (ref(eid) != reffromblocks(eid)) {
+        tmp = false
+      }
+    }
+    tmp
+  }
+  def printInfo() : Unit = {
+    println(s"PCATestData info: w=$w h=$h n=$n m=$m vecbw=$vecbw matbw=$matbw nblocks=$nblocks validate=${validateRef()}")
   }
 }
 
-object Temp extends App {
-  val a = Array(
-    Array(1.0, 2.0, 3.0, 4.0),
-    Array(5.0, 6.0, 7.0, 8.0)
-  )
-  val t = new PCATestData()
-  val result = t.hsplit(a, 2)
-  result.foreach { part =>
-    println("Split:")
-    part.foreach(row => println(row.mkString("[", ", ", "]")))
+
+class PCATestDataSpec extends AnyFlatSpec {
+  behavior of "PCATestData"
+
+  "Default config" should "pass" in {
+    val t = new PCATestData()
+    t.printInfo()
+    assert(t.validateRef())
+  }
+
+
+
+  "Large config" should "pass" in {
+    val t = new PCATestData(w=192, h=168, m=100, vecbw=12, matbw=8, nblocks=8)
+    t.printInfo()
+    assert(t.validateRef())
   }
 }
 
